503231b890e00e535564943d7831c2a5296d5acc
shawn <shawn922003@gmail.com>
2023-09-28 00:25:02 +0800

    修正bucket sort的bug

--- bucket_sort/Project1/Project1/Source.cpp
+++ bucket_sort/Project1/Project1/Source.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 
 void _insertion_sort(list<double>& A, int size) {
+	if (A.empty()) return;
+
 	list<double>::iterator now_node = ++A.begin();
 	for (int i = 1; i < size; ++i) {
 		double key = *now_node;
 		list<double>::iterator pre_node = now_node;
 
-		do {
-			--pre_node;
-		} while (pre_node != A.begin() && *pre_node>key);
 
-		A.insert(++pre_node, key);
+		while (pre_node != A.begin() && *--pre_node>key);
+		if (*pre_node <= key) ++pre_node;
+
+		A.insert(pre_node, key);
 
 		now_node=A.erase(now_node);
 
@@ -30,6 +32,8 @@ void bucket_sort(vector<double>& A, int size) {
 		int index = int(it * size);
 		B[index].push_back(it);
 	}
+
+	for (auto& it : B) _insertion_sort(it, it.size());
 
 	for (int i = 1; i < size; i++) B[0].splice(B[0].end(), B[i]);
 
@@ -46,8 +50,10 @@ void print(T A) {
 
 int main() {
 
-	vector<double> A = { 0.9,0.23,0.34,0.5632,0.66,0.77 };
+	vector<double> A = { 0.9,0.23,0.34,0.5632,0.66,0.77,0.78,0.75,0.73 };
 	bucket_sort(A, A.size());
+
+
 	print(A);
 	return 0;
 }
